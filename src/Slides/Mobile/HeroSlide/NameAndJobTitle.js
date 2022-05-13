edc79df8e895f71195022ddc401598b6d7df72e8
RaghavBhajipale <raghavbhajipale@gmail.com>
2022-05-13 19:02:48 +0530

    Configure React app for deployment to GitHub Pages

--- src/Slides/Mobile/HeroSlide/NameAndJobTitle.js
+++ src/Slides/Mobile/HeroSlide/NameAndJobTitle.js
@@ -9,7 +9,7 @@ const Container = styled.section`
     align-items: center;
     height:50vh;
     width:100%;
-    background-color: red;
+    background-color: white;
     /* border: 1px solid blue; */
 `;
 
